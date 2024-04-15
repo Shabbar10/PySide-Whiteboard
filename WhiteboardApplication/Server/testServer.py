2e95dbc5daa4932386c8961c08d4957c7571415a
Shabbar10 <adamjeeshabbar@gmail.com>
2024-04-15 16:13:18 +0530

    Main server implemented (again)

--- WhiteboardApplication/Server/testServer.py
+++ WhiteboardApplication/Server/testServer.py
@@ -1,16 +1,13 @@
 import redis
-import subprocess
 from PySide6.QtNetwork import QTcpServer, QTcpSocket, QHostAddress, QAbstractSocket
-from PySide6.QtCore import QCoreApplication
-import json
 from getip import get_local_ip
 from netManage import SignalManager
-from PySide6.QtCore import QCoreApplication, QTimer, Signal
+from PySide6.QtCore import QCoreApplication, Signal
 
-signalmg = SignalManager()
+signal_manager = SignalManager()
 
 
-class MyServer2(QTcpServer):
+class MyServer(QTcpServer):
     def __init__(self):
         super().__init__()
         self.timer = None
@@ -18,53 +15,36 @@ class MyServer2(QTcpServer):
         self.clients = []
         self.client_socket = []
         self.database_signal = Signal(str, str)
-        self.redis_host = 'localhost'
-        self.redis_port = 6379
+        redis_host = 'localhost'
+        redis_port = 5001
+
+        self.r = redis.StrictRedis(redis_host, redis_port, decode_responses=True)
 
     def incomingConnection(self, socket_descriptor):
         socket = QTcpSocket()
         socket.setSocketDescriptor(socket_descriptor)
 
-        socket.readyRead.connect(self.on_connected)
-        socket.disconnected.connect(self.on_disconnected)
-        print(f"Client connected from {socket.peerAddress().toString()}")
+        self.counter += 1
+        username = "User" + str(self.counter)
+        self.r.hset(username, 'IP', socket.peerAddress().toString())
+
+        print(self.r.hgetall(username))
 
         self.client_socket.append(socket)
-        self.clients.append(socket.peerAddress().toString())
+        for each_socket in self.client_socket:
+            each_socket.readyRead.connect(self.on_connected)
 
-        print("Yes")
+        socket.disconnected.connect(self.on_disconnected)
 
     def on_connected(self):
-        socket = self.sender()
-        ip_add = socket.peerAddress().toString()
-        self.counter += 1
+        sender_socket = self.sender()
+        sender_ip = sender_socket.peerAddress().toString()
 
-        print(ip_add)
-        username = "User" + str(self.counter)
-        print("Update DB called")
-        try:
-            r = redis.StrictRedis(host=self.redis_host, port=self.redis_port, decode_responses=True)
-            # r.set('India', 'Mumbai')
-            r.hset(username, 'IP', ip_add)
-            print(f"Registered User : {r.hgetall(username)}")
+        data = sender_socket.readAll().data()
 
-        except Exception as e:
-            print(f"ERROR OCCURRED : {e}")
-        # self.database_signal.emit(username, ip_add)
-
-    def update_db(self, username: str, ip: str):
-        print("Update DB called")
-        try:
-            r = redis.StrictRedis(host=self.redis_host, port=self.redis_port, decode_responses=True)
-            # r.set('India', 'Mumbai')
-            r.hset(username, 'IP', ip)
-            print(f"Registered User : {r.hgetall(username)}")
-
-        except Exception as e:
-            print(f"ERROR OCCURRED : {e}")
-
-    def read_from_db(self):
-        pass
+        for each_socket in self.client_socket:
+            if each_socket.peerAddress().toString() != sender_ip:
+                each_socket.write(data)
 
     def on_disconnected(self):
         socket = self.sender()
@@ -76,19 +56,18 @@ class MyServer2(QTcpServer):
         # QCoreApplication.processEvents()
 
 
-def start_server(server: MyServer2):
+def start_server(server: MyServer):
     SERVER_IP = get_local_ip()
     server.listen(QHostAddress(SERVER_IP), 8080)
     if server.isListening():
-        print("Server is listening on port 5000, IP : ", SERVER_IP)
+        print("Server is listening on port 8080, IP : ", SERVER_IP)
     else:
         print("Server could not start. Error:", server.errorString())
 
 
 if __name__ == "__main__":
     app = QCoreApplication([])
-    local_server = MyServer2()
-    signalmg.send_info2.connect(local_server.update_db)
+    local_server = MyServer()
 
     start_server(local_server)
     app.exec()
